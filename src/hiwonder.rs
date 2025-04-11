fb199ee07444b218b66ce5d241901bbd7212e5e1
Benjamin Bolte <ben@bolte.cc>
2025-04-10 23:32:37 -0700

    slight improvement

--- src/hiwonder.rs
+++ src/hiwonder.rs
@@ -81,9 +81,9 @@ impl HALIMU for KBotIMU {
             accel_y: (data.accelerometer[1] as f64) * 9.81,
             accel_z: (data.accelerometer[2] as f64) * 9.81,
             // Raw units from IMU are in deg/s / 2000.0., convert to rad/s
-            gyro_x: (data.gyroscope[0] as f64) * 2000.0 * 3.14159 / 180.0,
-            gyro_y: (data.gyroscope[1] as f64) * 2000.0 * 3.14159 / 180.0,
-            gyro_z: (data.gyroscope[2] as f64) * 2000.0 * 3.14159 / 180.0,
+            gyro_x: (data.gyroscope[0] as f64) * (2000.0 * 3.14159 / 180.0),
+            gyro_y: (data.gyroscope[1] as f64) * (2000.0 * 3.14159 / 180.0),
+            gyro_z: (data.gyroscope[2] as f64) * (2000.0 * 3.14159 / 180.0),
             mag_x: None,
             mag_y: None,
             mag_z: None,
